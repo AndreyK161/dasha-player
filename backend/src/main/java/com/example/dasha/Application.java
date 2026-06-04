8716919ecf759d74faf98d3dedf5553431f13a9a
AndreyK161 <moonandre9999@mail.ru>
2026-06-05 00:33:35 +0300

    fix: теперь запускается при запуске сервака

--- backend/src/main/java/com/example/dasha/Application.java
+++ backend/src/main/java/com/example/dasha/Application.java
@@ -1,13 +1,27 @@
 package com.example.dasha;
 
+import com.example.dasha.services.PlayerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class Application {
 
+    private final PlayerService playerService;
+
+    public Application(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onStart() {
+        playerService.startPlaylist();
     }
 
 }
